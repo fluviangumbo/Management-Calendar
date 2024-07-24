b089899e5694c202f4190870865e6554b1c5d62c
dTop <jroney002@gmail.com>
2024-07-23 21:55:40 -0500

    daytasks and emp rendering functions

--- assets/js/day.js
+++ assets/js/day.js
@@ -1,29 +1,58 @@
-
 const day = new URLSearchParams(location.search).get("day")
 const backBtn = document.querySelector('#back');
 // pulls data 
-const allTasks = pullTaskData()
+const allTasks = pullTaskData();
+const allEmps = pullEmpData();
+const taskEl = document.querySelector('#taskDisplay');
 
-const currentDayTasks = allTasks.filter(function(task){
-    return task.day === day 
-})
+const currentDayTasks = allTasks.filter( function (task) {
+  return task.day === day 
+});
 
-function fetchTaskEmps(){
-    const allTasks= pullTaskData();
-    const allEmps= pullTaskData();
+currentDayTasks.forEach(function (task) {
+  taskBuilder('div', task, taskEl);
+});
+
+
+function taskBuilder (type, task, parentEl) {
+  const elem = document.createElement(type);
+  const taskName = task.task.value;
+  const taskStart = `${task.startHr.value}:${task.startMin.value}`;
+  const taskDuration = task.durattion.value;
+
+  const title = document.createElement('h2');
+  title.textContent = taskName;
+
+  const info = document.createElement('p');
+  info.textContent = `Start Time: ${taskStart}  Duration: ${taskDuration}`;
+
+  elem.appendChild(title);
+  elem.appendChild(info);
+
+  fetchTaskEmps(elem, task);
+  
+  parentEl.appendChild(elem);
 }
-// select task for that day 
-    for(let i=0; i<allEmps.lengths;i++){
-  if(task.assigned.includes(i)){
-    allEmps[i]
-    empBuilder(div,allEmps[i],empEl)
-  }
+
+function fetchTaskEmps(empEl, dayTask) { // right now we need a way to validate the day 
+    // select task for that day 
+    for(let i = 0; i < allEmps.lengths; i++) {
+      if (dayTask.assigned.includes(i)) {
+        allEmps[i];
+        empBuilder('div', allEmps[i], empEl);
+      }
     }
+}
 
-let redirectURL = 'index.html';
+function empBuilder (type, empIndexed, parentEl) {
+  const elem = document.createElement(type);
+  elem.textContent = `${empIndexed.firstName} ${empIndexed.lastName} assigned.`;
+  parentEl.appendChild(elem);
+}
+    
 
-const redirectPage = function (url) {
-    redirectURL = url;
+const redirectPage = function () {
+    const redirectURL = 'index.html';
     location.assign(url);
   };
 
