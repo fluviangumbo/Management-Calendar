d8c3d5b89c6d8107439a0e97187d8a407df01944
bungin <nimai.band@gmail.com>
2024-07-23 21:14:40 -0500

    fixed syntax on fetchTaskEmps(), back btn now works, submit doesnt.

--- assets/js/day.js
+++ assets/js/day.js
@@ -1,32 +1,31 @@
 
-const day = new URLSearchParams(location.search).get("day")
+const day = new URLSearchParams(location.search).get("day");
 const backBtn = document.querySelector('#back');
 // pulls data 
-const allTasks = pullTaskData()
+const allTasks = pullTaskData();
 
-const currentDayTasks = allTasks.filter(function(task){
-    return task.day === day 
-})
+const currentDayTasks = allTasks.filter(function (task) {
+    return task.day === day
+});
 
-function fetchTaskEmps(){
-    const allTasks= pullTaskData();
-    const allEmps= pullTaskData();
-}
-// select task for that day 
-    for(let i=0; i<allEmps.lengths;i++){
-  if(task.assigned.includes(i)){
-    allEmps[i]
-    empBuilder(div,allEmps[i],empEl)
-  }
+function fetchTaskEmps() {
+    const allTasks = pullTaskData();
+    const allEmps = pullTaskData();
+
+for (let i = 0; i < allEmps.lengths; i++) {
+    if (task.assigned.includes(i)) {
+        allEmps[i]
+        empBuilder(div, allEmps[i], empEl)
     }
+}};
 
-let redirectURL = 'index.html';
 
+let redirectURL = '';
 const redirectPage = function (url) {
     redirectURL = url;
     location.assign(url);
-  };
+};
 
 
-backBtn.addEventListener('click', function() {redirectPage('index.html')}); 
+backBtn.addEventListener('click', function() {redirectPage('index.html')});
 
