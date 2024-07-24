9aabf222e010424f908c89acf9a1ae911ae0f0ad
thunyP <tpt8669@gmail.com>
2024-07-23 20:21:25 -0500

    pulling from emp and task and day select

--- assets/js/day.js
+++ assets/js/day.js
@@ -1,7 +1,18 @@
 const day = new URLSearchParams(location.search).get("day")
 console.log(day)
+// pulls data 
+const allTasks = pullTaskData()
+const currentDayTasks = allTasks.filter(function(task){
+    return task.day === day 
+})
 
-
-
+function fetchTaskEmps(){
+    const allTasks= pullTaskData();
+    const allEmps= pullTaskData();
+}
+// select task for that day 
+    for(let i=0; i<allEmps.lengths;i++){
+  
+    }
 
 
